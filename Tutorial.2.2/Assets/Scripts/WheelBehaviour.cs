861c697401555c1f143de1f8c7f4696cd5336716
Beastlike3773 <38509042+Beastlike3773@users.noreply.github.com>
2020-02-19 23:27:23 +0100

    Added scripts

---
+++ Tutorial.2.2/Assets/Scripts/WheelBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelBehaviour : MonoBehaviour
+{
+    public WheelCollider wheelCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the wheel position and rotation from the wheelcolider
+        Quaternion quat;
+        Vector3 position;
+        wheelCol.GetWorldPose(out position, out quat);
+        transform.position = position;
+        transform.rotation = quat;
+    }
+}
