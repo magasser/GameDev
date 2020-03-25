fa921ea1d681409013542fad5a4eb50818aa17dc
Beastlike3773 <38509042+Beastlike3773@users.noreply.github.com>
2020-03-25 10:12:35 +0100

    Added different Gear Types

--- Tutorial.2.2/Assets/Scripts/CarBehaviour.cs
+++ Tutorial.2.2/Assets/Scripts/CarBehaviour.cs
@@ -3,16 +3,20 @@ using System.Collections;
 using TMPro;
 using System;
 
+public enum GearType
+{
+    Comfort,
+    Sport
+}
+
 public class CarBehaviour : MonoBehaviour
 {
     public WheelCollider wheelFL;
     public WheelCollider wheelFR;
     public WheelCollider wheelRR;
     public WheelCollider wheelRL;
-    public float maxTorque = 500;
+    public GearType gearType;
     public float maxSteerAngle = 45;
-    public float maxSpeedKMH = 150;
-    public float maxSpeedBackwardKMH = 30;
     public float forwardFriction;
     public float sidewaysFriction;
     public Transform centerOfMass;
@@ -29,6 +33,12 @@ public class CarBehaviour : MonoBehaviour
     private Rigidbody _rigidBody;
     private float _currentSpeedKMH;
     private int _currentGear;
+    private float _maxTorque;
+    private float _maxBreakTorqueFront;
+    private float _maxBreakTorqueRear;
+    private float _torqueReduction;
+    private float _maxSpeedKMH;
+    private float _maxSpeedBackwardKMH;
 
     void Start()
     {
@@ -48,6 +58,35 @@ public class CarBehaviour : MonoBehaviour
         _engineAudioSource.playOnAwake = true;
         _engineAudioSource.enabled = false; // Bugfix
         _engineAudioSource.enabled = true; // Bugfix
+
+        switch (gearType)
+        {
+            case GearType.Sport:
+                _maxTorque = 1300;
+                _maxBreakTorqueFront = 2000;
+                _maxBreakTorqueRear = 1500;
+                _torqueReduction = 80;
+                _maxSpeedKMH = 180;
+                _maxSpeedBackwardKMH = 20;
+                break;
+            case GearType.Comfort:
+                _maxTorque = 800;
+                _maxBreakTorqueFront = 1300;
+                _maxBreakTorqueRear = 1000;
+                _torqueReduction = 120;
+                _maxSpeedKMH = 150;
+                _maxSpeedBackwardKMH = 20;
+                break;
+            default:
+                _maxTorque = 900;
+                _maxBreakTorqueFront = 1300;
+                _maxBreakTorqueRear = 1000;
+                _torqueReduction = 120;
+                _maxSpeedKMH = 150;
+                _maxSpeedBackwardKMH = 20;
+                break;
+
+        }
     }
     void FixedUpdate()
     {
@@ -64,10 +103,21 @@ public class CarBehaviour : MonoBehaviour
 
         if (doBraking)
         {
-            wheelFL.brakeTorque = 1500;
-            wheelFR.brakeTorque = 1500;
-            wheelRL.brakeTorque = 1000;
-            wheelRR.brakeTorque = 1000;
+            if(velocityIsForeward)
+            {
+                wheelFL.brakeTorque = -_maxBreakTorqueFront * Input.GetAxis("Vertical");
+                wheelFR.brakeTorque = wheelFL.brakeTorque;
+                wheelRL.brakeTorque = -_maxBreakTorqueRear * Input.GetAxis("Vertical");
+                wheelRR.brakeTorque = wheelRL.brakeTorque;
+            }
+            else
+            {
+                wheelFL.brakeTorque = _maxBreakTorqueFront * Input.GetAxis("Vertical");
+                wheelFR.brakeTorque = wheelFL.brakeTorque;
+                wheelRL.brakeTorque = _maxBreakTorqueRear * Input.GetAxis("Vertical");
+                wheelRR.brakeTorque = wheelRL.brakeTorque;
+            }
+
             wheelFL.motorTorque = 0;
             wheelFR.motorTorque = 0;
         }
@@ -77,14 +127,14 @@ public class CarBehaviour : MonoBehaviour
             wheelFR.brakeTorque = 0;
             wheelRL.brakeTorque = 0;
             wheelRR.brakeTorque = 0;
-
-            if (velocityIsForeward && _currentSpeedKMH < maxSpeedKMH)
+            float torque = _maxTorque - (_currentGear * _torqueReduction);
+            if (velocityIsForeward && _currentSpeedKMH < _maxSpeedKMH)
             {
-                SetMotorTorque(maxTorque * Input.GetAxis("Vertical"));
+                SetMotorTorque(torque * Input.GetAxis("Vertical"));
             }
-            else if (!velocityIsForeward && _currentSpeedKMH < maxSpeedBackwardKMH)
+            else if (!velocityIsForeward && _currentSpeedKMH < _maxSpeedBackwardKMH)
             {
-                SetMotorTorque(maxTorque * Input.GetAxis("Vertical"));
+                SetMotorTorque(torque * Input.GetAxis("Vertical"));
             }
             else
             {
@@ -98,7 +148,7 @@ public class CarBehaviour : MonoBehaviour
         SetSteerAngle(steerAngle);
 
         int gearNum = 0;
-        float engineRPM = kmh2rpm(_currentSpeedKMH, out gearNum);
+        float engineRPM = kmh2rpm(_currentSpeedKMH, out gearNum, gearType);
         _currentGear = gearNum;
         SetEngineSound(engineRPM);
     }
@@ -153,6 +203,7 @@ public class CarBehaviour : MonoBehaviour
             _minKMH = minKMH;
             _maxRPM = maxRPM;
             _maxKMH = maxKMH;
+
         }
 
         public bool speedFits(float kmh)
@@ -162,21 +213,51 @@ public class CarBehaviour : MonoBehaviour
 
         public float interpolate(float kmh)
         {
-            return Math.Max(kmh * _maxRPM / _maxKMH, _minRPM);
+            return ((kmh - _minKMH) * (_maxRPM - _minRPM) / (_maxKMH - _minKMH)) + _minRPM;
         }
     }
 
-    float kmh2rpm(float kmh, out int gearNum)
+    float kmh2rpm(float kmh, out int gearNum, GearType gearType)
     {
-        Gear[] gears =
+        Gear[] gears;
+
+        switch (gearType)
         {
-            new Gear( 1, 900, 12, 1400),
-            new Gear( 12, 900, 25, 2000),
-            new Gear( 25, 1350, 45, 2500),
-            new Gear( 45, 1950, 70, 3500),
-            new Gear( 70, 2500, 112, 4000),
-            new Gear(112, 3100, 180, 5000)
-        };
+            case GearType.Sport:
+                gears = new Gear[]
+                {
+                    new Gear(1, 700, 40, 5200),
+                    new Gear(40, 3000, 72, 5600),
+                    new Gear(72, 3800, 110, 6100),
+                    new Gear(110, 4700, 151, 6500),
+                    new Gear(151, 5300, 197, 7100),
+                    new Gear(197, 5400, 180, 8000)
+                };
+                break;
+            case GearType.Comfort:
+                gears = new Gear[]
+                {
+                    new Gear(1, 900, 12, 1400),
+                    new Gear(12, 900, 25, 2000),
+                    new Gear(25, 1350, 45, 2500),
+                    new Gear(45, 1950, 70, 3500),
+                    new Gear(70, 2500, 112, 4000),
+                    new Gear(112, 3100, 180, 5000)
+                };
+                break;
+            default:
+                gears = new Gear[]
+                {
+                    new Gear( 1, 900, 12, 1400),
+                    new Gear( 12, 900, 25, 2000),
+                    new Gear( 25, 1350, 45, 2500),
+                    new Gear( 45, 1950, 70, 3500),
+                    new Gear( 70, 2500, 112, 4000),
+                    new Gear(112, 3100, 180, 5000)
+                };
+                break;
+        }
+
         for (int i = 0; i < gears.Length; ++i)
         {
             if (gears[i].speedFits(kmh))
@@ -196,7 +277,7 @@ public class CarBehaviour : MonoBehaviour
         float maxRPM = 8000;
         float minPitch = 0.3f;
         float maxPitch = 3.0f;
-        float pitch = Math.Max(engineRPM * maxPitch / maxRPM, minPitch);
+        float pitch = ((engineRPM - minRPM) * (maxPitch - minPitch) / (maxRPM - minRPM)) + minPitch;
         _engineAudioSource.pitch = pitch;
     }
 }
